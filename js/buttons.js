58af340aa223f4cacdff7c9dad55de8dd594b9ac
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-02-26 14:36:06 +0000

    button logic converted to switch statement

--- js/buttons.js
+++ js/buttons.js
@@ -5,8 +5,57 @@ const screen = document.getElementsByClassName('screen')[0];
     console.log(element);
 });*/
 let historyInput = [];
+const re = /([^\=]+$)/;
+
 Array.from(btns).forEach(child => {
     child.addEventListener('click', (e) => {
+      switch(e.target.value) {
+        case 'C':
+          screen.innerHTML = "";
+          break;
+        case 'sign':
+          if(!isNaN(screen.innerHTML)) {
+            var number = Number(screen.innerHTML);
+            number = -number;
+            screen.innerHTML = number.toString();
+          }
+          break;
+        case '=':
+          historyInput.push(e.target.value);
+          console.log(historyInput);
+          if (screen.innerHTML.includes(".")) {
+            var x = (eval(screen.innerHTML));
+            screen.innerHTML = x.toPrecision(screen.innerHTML.split(".")[1].length).toString();
+            historyInput.push(screen.innerHTML);
+          }
+          else {
+            screen.innerHTML = (eval(screen.innerHTML));
+            historyInput.push(screen.innerHTML);
+          }
+          console.log(historyInput);
+          break;
+        case 'back':
+          /*if(historyInput[historyInput.length-1] == '=') {
+            while(historyInput[historyInput.length-1] == '=') {
+              historyInput.pop();
+            }
+          } else {
+            historyInput.pop();
+          }
+          screen.innerHTML = historyInput.join("");*/
+          historyInput = historyInput.slice(0,-2);
+          let lastOperation = historyInput.join("").match(re);
+          
+          screen.innerHTML = lastOperation[0];
+          break;
+        default:
+          screen.innerHTML += ( e.target.value );
+          historyInput.push(e.target.value);
+          console.log(historyInput);
+          break;
+      }
+      /*
+
       if(e.target.value == 'C') {
         screen.innerHTML = "";
       }
@@ -45,7 +94,7 @@ Array.from(btns).forEach(child => {
         screen.innerHTML += ( e.target.value );
         historyInput.push(e.target.value);
         console.log(historyInput);
-      }
+      }*/
     } );
 
 });
