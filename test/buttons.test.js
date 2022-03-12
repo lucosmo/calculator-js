f25dc620fa5287749de023d98dd80c691feb02fd
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-03-12 14:42:36 +0000

    more unit tests and coverage report 21.27%

--- test/buttons.test.js
+++ test/buttons.test.js
@@ -3,6 +3,12 @@ const bfunctions = require('../js/buttons');
 test('check if last number in "1+3+34" is 34', () => {
   expect(bfunctions.takeLastNumber("1+3+34")[0]).toBe('34');
 });
+test('check if last number in "1" is 1', () => {
+  expect(bfunctions.takeLastNumber("1")[0]).toBe('1');
+});
+test('check if last number in "1-dagdhj" is 1', () => {
+  expect(bfunctions.takeLastNumber("1")[0]).toBe('1');
+});
 test('check if last number in "1+3+34fd" is 3', () => {
   expect(bfunctions.takeLastNumber("1+3+34fd")[0]).toBe('3');
 });
