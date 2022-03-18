e9492e9c765e80f23aaf90758de99263de6e18e7
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-03-18 15:17:55 +0000

    Update button_logic.js

--- js/button_logic.js
+++ js/button_logic.js
@@ -1,3 +1,7 @@
+/*
+ * @param object Object containing screen's html and history of clicked buttons
+ * 
+*/
 export const clean_screen = calculatorData => {
   calculatorData.screen = '';
   calculatorData.history = [];
