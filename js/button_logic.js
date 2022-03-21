658caf9f2ab9f746a93c41756e2842303c891b45
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-03-21 13:20:20 +0000

    Doc comment to clear_screen

--- js/button_logic.js
+++ js/button_logic.js
@@ -1,6 +1,6 @@
 /*
  * @param object Object containing screen's html and history of clicked buttons
- * 
+ * @return object Returns modified object with reset values
 */
 export const clean_screen = calculatorData => {
   calculatorData.screen = '';
