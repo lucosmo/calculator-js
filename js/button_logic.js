4ea9a44f17b9a914f770026ff9155e2d853e38dd
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-03-24 12:24:33 +0000

    doc for change_sign

--- js/button_logic.js
+++ js/button_logic.js
@@ -9,12 +9,15 @@ export const clean_screen = calculatorData => {
   return calculatorData;
 }
 
-
-export const change_sign = calculatorData => {
+/*
+ * @param object Object containing screen's html and history of clicked buttons
+ * @return object Returns modified object with changed sign (-/+) of number (object.screen)
+*/
+  export const change_sign = calculatorData => {
   if(isNumeric(calculatorData.screen)){
     let number = Number(calculatorData.screen);
     number = -number;
     calculatorData.screen = number.toString();
   }
   return calculatorData;
-}
+  }
