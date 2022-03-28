eccb87c22cc9983f8ecab0625de91d11ddb06e30
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-03-28 12:53:39 +0100

    TakeNumber function moved to helper.js

--- js/helpers.js
+++ js/helpers.js
@@ -8,3 +8,18 @@
 export function isNumeric(s) {
   return !isNaN(parseFloat(s)) && isFinite(s);
 }
+
+/**
+  * Function takes string as argument, matches regex rule against argument
+  * and returns result.
+  *
+  * @param {string} s Calculator output (screen)
+  *
+  * @return {array} Return array as matched regex
+  * @return {null} If regex isn't matched
+  */
+export const takeLastNumber = s => {
+  const reMath = /([^\+\-\*\/]+$)/;
+  const reMath2 = /.*(\d)$/
+  return s.match(reMath);
+};
