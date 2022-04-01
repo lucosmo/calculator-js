7366bfff95b387c5977e3391713b2ee458a130d2
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-04-01 14:26:41 +0100

    test calc_percent no2

--- test/buttons.test.js
+++ test/buttons.test.js
@@ -17,3 +17,7 @@ let x = calculatorData("3%","3%");
 test('check if percent of 3 (3%) is 0.03', (x) => {
   expect(bfunctions.calc_percent(x)[0]).toBe('0.03');
 });
+let x = calculatorData("1+3%","1+3%");
+test('check if 1+3% is 1+0.03', (x) => {
+  expect(bfunctions.calc_percent(x)[0]).toBe('1+0.03');
+});
