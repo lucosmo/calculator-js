c210713aa297994249bbe83ca5b30c12c9b63098
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-06-19 16:10:44 +0100

    blueprint for getNumber function taking and splitting calculator screen

---
+++ js/numbers.js
@@ -0,0 +1,10 @@
+/**
+* Takes calculator screen and turns it into array of numbers
+*
+* @param {string} calculator_screen Calculator output (screen)
+* @return {array} array of numbers (integers)
+
+*/
+export function getNumber(calculator_screen){
+
+}
