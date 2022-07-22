97fb99224f423f17f2c16cc06c177e37d8c23e5e
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-07-22 21:45:06 +0100

    Updated floatMultiply, checking if params are ints

--- js/numbers.js
+++ js/numbers.js
@@ -40,5 +40,8 @@ export function getNumber(numbers){
 */
 
 export function floatMultiply(num1, num2){
-  return num1[0]*num2[0]/(num1[1]*num2[2]);
+  if num1[1] == 0 and num2[1] == 0:
+     return num1[0]*num2[0]
+  else:
+     return num1[0]*num2[0]/(num1[1]*num2[2]);
 }
