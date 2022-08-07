0490357fdff459ae6c2124b299edb29dc81ec05f
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-08-07 19:20:48 +0100

    Fixed syntax

--- js/numbers.js
+++ js/numbers.js
@@ -40,8 +40,10 @@ export function getNumber(numbers){
 */
 
 export function floatMultiply(num1, num2){
-  if num1[1] == 0 and num2[1] == 0:
-     return num1[0]*num2[0]
-  else:
+  if (num1[1] == 0 and num2[1] == 0){
+     return num1[0]*num2[0];
+  }
+  else{
      return num1[0]*num2[0]/10**(num1[1]*num2[2]);
+  }
 }
